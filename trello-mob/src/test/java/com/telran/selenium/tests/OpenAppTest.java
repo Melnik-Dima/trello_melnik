345d712ef41461ef9bcc3957a6fc6ce0d795afc0
Dmitry Melnik <familymelniks@gmail.com>
2019-11-12 19:33:15 +0200

    BoardCreationWrongConfirmLocator

---
+++ trello-mob/src/test/java/com/telran/selenium/tests/OpenAppTest.java
@@ -0,0 +1,15 @@
+package com.telran.selenium.tests;
+
+import org.testng.annotations.Test;
+
+public class OpenAppTest extends TestBase {
+
+    @Test
+    public void openAppTest() throws InterruptedException {
+        System.out.println("App opened");
+
+
+    }
+
+
+}
